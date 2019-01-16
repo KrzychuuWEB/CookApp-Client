836586049d2f5139155dc925da10b3df3c8addd3
Krzysztof <KrzychuuWEB@gmail.com>
2019-01-16 22:02:57 +0100

    Create validation for login and register page

--- src/views/Security/Login/index.js
+++ src/views/Security/Login/index.js
@@ -12,6 +12,7 @@ import Input from "@material-ui/core/es/Input/Input";
 import InputAdornment from "@material-ui/core/es/InputAdornment/InputAdornment";
 import IconButton from "@material-ui/core/es/IconButton/IconButton";
 import {Visibility, VisibilityOff} from "@material-ui/icons";
+import FormHelperText from "@material-ui/core/es/FormHelperText/FormHelperText";
 
 const styles = theme => ({
     root: {
@@ -48,10 +49,51 @@ const styles = theme => ({
 class Login extends Component {
     state = {
         showPassword: false,
+        login: '',
+        password: '',
+        errors: {},
     };
 
     handleClickShowPassword = () => {
         this.setState(state => ({ showPassword: !state.showPassword }));
+    };
+
+    validation = () => {
+        let { login, password } = this.state;
+        let errors = {};
+        let isError = false;
+
+        if(login.length < 1) {
+            isError = true;
+            errors.login = "Pole jest wymagane!"
+        }
+
+        if(password.length < 1) {
+            isError = true;
+            errors.password = "Pole jest wymagane!"
+        } else if(password.length < 8) {
+            isError = true;
+            errors.password = "Hasło musi mieć minimum 8 znaków!"
+        }
+
+        this.setState({errors: errors});
+        return isError;
+    };
+
+    onChange = input => e => {
+        this.setState({
+            [input]: e.target.value,
+        })
+    };
+
+    onClick = () => {
+        let checkError = this.validation();
+
+        if(!checkError) {
+            return true;
+        } else {
+            return false;
+        }
     };
 
     render() {
@@ -67,14 +109,20 @@ class Login extends Component {
                     <form noValidate autoComplete="off">
                         <div className={classes.container}>
                             <TextField
+                                onChange={this.onChange("login")}
+                                value={this.state.login}
                                 className={classes.textField}
                                 id="login"
                                 label="Login"
                                 name="user_login"
+                                error={!!this.state.errors.login}
+                                helperText={this.state.errors.login}
                             />
-                            <FormControl className={classes.textField}>
+                            <FormControl className={classes.textField} error={!!this.state.errors.password}>
                                 <InputLabel htmlFor="adorment-password">Hasło</InputLabel>
                                 <Input
+                                    onChange={this.onChange("password")}
+                                    value={this.state.password}
                                     name="user_password"
                                     id="adorment-password"
                                     type={this.state.showPassword ? 'text' : 'password'}
@@ -89,12 +137,17 @@ class Login extends Component {
                                         </InputAdornment>
                                     }
                                 />
+                                <FormHelperText>{this.state.errors.password}</FormHelperText>
                             </FormControl>
                         </div>
                         <div className={classes.buttonDiv}>
                             <Button style={{marginLeft: 20}} component={Link} to="/register" variant="text" color="secondary">Rejestracja</Button>
 
-                            <Button variant="contained" color="primary">Zaloguj</Button>
+                            <Button
+                                onClick={this.onClick}
+                                variant="contained"
+                                color="primary"
+                            >Zaloguj</Button>
                         </div>
                     </form>
                 </Paper>
--- src/views/Security/Register/index.js
+++ src/views/Security/Register/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './register.scss';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from "@material-ui/core/es/Paper/Paper";
@@ -13,6 +14,7 @@ import IconButton from "@material-ui/core/es/IconButton/IconButton";
 import {Visibility, VisibilityOff} from "@material-ui/icons";
 import FormControlLabel from "@material-ui/core/es/FormControlLabel/FormControlLabel";
 import Checkbox from "@material-ui/core/es/Checkbox/Checkbox";
+import FormHelperText from "@material-ui/core/es/FormHelperText/FormHelperText";
 
 const styles = theme => ({
     root: {
@@ -50,18 +52,103 @@ const styles = theme => ({
 
 class Register extends Component {
     state = {
+        values: {
+            login: '',
+            email: '',
+            password: '',
+            repeatPassword: '',
+            terms: false,
+        },
         showPassword: false,
+        errors: {},
+    };
+
+    checkEmail = email => {
+        let regex = /\S+@\S+\.\S+/;
+        return regex.test(email);
     };
 
     handleClickShowPassword = () => {
         this.setState(state => ({ showPassword: !state.showPassword }));
     };
 
+    onChange = input => e => {
+        this.setState({
+            values: { ...this.state.values, [input]: e.target.value }
+        });
+    };
+
+    handleCheckbox = e => {
+      this.setState({
+          values: { ...this.state.values, terms: e.target.checked}
+      })
+    };
+
+    validation = () => {
+        let { values } = this.state;
+        let errors = {};
+        let isError = false;
+
+        if(values.login.length < 1) {
+            isError = true;
+            errors.login = "Pole jest wymagane!";
+        }
+
+        if(values.email.length < 1) {
+            isError = true;
+            errors.email = "Pole jest wymagane!";
+        } else if(!this.checkEmail(values.email)) {
+            isError = true;
+            errors.email = "Email jest nieprawidłowy!";
+        }
+
+        if(values.password.length < 1) {
+            isError = true;
+            errors.password = "Pole jest wymagane!"
+        } else if(values.password.length < 8) {
+            isError = true;
+            errors.password = "Hasło musi mieć minimum 8 znaków!"
+        }
+
+        if(values.repeatPassword.length < 1) {
+            isError = true;
+            errors.repeatPassword = "Pole jest wymagane!";
+        } else if(values.repeatPassword.length < 8) {
+            isError = true;
+            errors.repeatPassword = "Hasło musi mieć minimum 8 znaków!"
+        }
+
+        if (values.repeatPassword !== values.password) {
+            isError = true;
+            errors.password = "Hasła nie są takie same!";
+            errors.repeatPassword = "Hasła nie są takie same!";
+        }
+
+        if(!values.terms) {
+            isError = true;
+            errors.terms = "Regulamin jest wymagany!"
+        }
+
+        this.setState({errors: errors});
+        return isError;
+    };
+
+    onClick = () => {
+        let checkError = this.validation();
+
+        if(!checkError) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
     render() {
         const { classes } = this.props;
+        const { errors, values } = this.state;
 
         return (
-            <div>
+            <div className="register-container">
                 <Paper className={classes.root} elevation={1}>
                     <Typography variant="h6" color="secondary">
                         Zarejestruj się!
@@ -69,20 +156,27 @@ class Register extends Component {
 
                     <form className={classes.container} noValidate autoComplete="off">
                         <TextField
+                            value={values.login}
                             className={classes.widthBox}
                             label="Login"
+                            onChange={this.onChange("login")}
+                            error={!!errors.login}
+                            helperText={errors.login}
                         />
                         <TextField
+                            value={values.email}
                             className={classes.widthBox}
                             label="Email"
+                            onChange={this.onChange("email")}
+                            error={!!errors.email}
+                            helperText={errors.email}
                         />
-                        <TextField
-                            className={classes.widthBox}
-                            label="Email"
-                        />
-                        <FormControl className={classes.widthBox}>
+
+                        <FormControl className={classes.widthBox} error={!!errors.password}>
                             <InputLabel htmlFor="adorment-password">Hasło</InputLabel>
                             <Input
+                                value={values.password}
+                                onChange={this.onChange("password")}
                                 id="adorment-password"
                                 type={this.state.showPassword ? 'text' : 'password'}
                                 endAdornment={
@@ -96,23 +190,46 @@ class Register extends Component {
                                     </InputAdornment>
                                 }
                             />
+                            <FormHelperText className="remove-margin">{ errors.password }</FormHelperText>
                         </FormControl>
-                        <FormControl className={classes.widthBox}>
+
+                        <FormControl className={classes.widthBox} error={!!errors.repeatPassword}>
                             <InputLabel htmlFor="repeat-password">Powtórz hasło</InputLabel>
                             <Input
+                                value={values.repeatPassword}
+                                onChange={this.onChange("repeatPassword")}
                                 id="repeat-password"
                                 type={this.state.showPassword ? 'text' : 'password'}
                             />
+                            <FormHelperText className="remove-margin">{ errors.repeatPassword }</FormHelperText>
                         </FormControl>
-                        <FormControlLabel className={classes.widthBox} control={
-                            <Checkbox
-                                value="accept_terms"
-                                color="primary"
-                            />
-                        } label="Akceptuje regulamin"/>
+
+                        <div className="terms-container">
+                            <FormControlLabel className={classes.widthBox} control={
+                                <Checkbox
+                                    onChange={this.handleCheckbox}
+                                    checked={values.terms}
+                                    value="accept_terms"
+                                    color="primary"
+                                />
+                            } label="Akceptuje regulamin" />
+
+                            {
+                                errors.terms ?
+                                    <Typography variant="caption" color="error">
+                                        { errors.terms }
+                                    </Typography>
+                                    : null
+                            }
+                        </div>
 
                         <div className={classes.buttonDiv}>
-                            <Button className={classes.button} variant="contained" color="primary">Zarejestruj</Button>
+                            <Button
+                                onClick={this.onClick}
+                                className={classes.button}
+                                variant="contained"
+                                color="primary"
+                            >Zarejestruj</Button>
                         </div>
                     </form>
                 </Paper>
@@ -126,3 +243,4 @@ Register.propTypes = {
 };
 
 export default withStyles(styles)(Register);
+

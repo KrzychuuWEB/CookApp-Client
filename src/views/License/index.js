0e1b6959ac5d9139c42edd927160724d7a667d32
Krzysztof <KrzychuuWEB@gmail.com>
2019-03-20 16:50:24 +0100

    Create component for license

--- src/views/License/index.js
+++ src/views/License/index.js
@@ -1,178 +1,52 @@
 import React, { Component } from 'react';
-import './license.scss';
-import {Paper, Typography} from "@material-ui/core";
+import LicenseCard from "../../components/License";
 
 class License extends Component {
     render() {
         return (
-            <div className="license-container">
-                <Paper className="paper">
-                    <div className="license-name">
-                        <Typography variant="headline" color="secondary">
-                            Axios ( <a rel="noopener noreferrer" target="_blank" href="https://github.com/axios/axios">GitHub</a>)
-                        </Typography>
-                    </div>
+            <div>
+                <LicenseCard
+                    licenseName="React"
+                    href="https://github.com/facebook/react"
+                >
+                    Copyright (c) Facebook, Inc. and its affiliates.
+                </LicenseCard>
 
-                    <div className="license-text">
-                        <Typography variant="caption">
-                            Copyright (c) 2014-present Matt Zabriskie<br/><br/>
+                <LicenseCard
+                    licenseName="Axios"
+                    href="https://github.com/axios/axios"
+                >
+                    Copyright (c) 2014-present Matt Zabriskie
+                </LicenseCard>
 
-                            Permission is hereby granted, free of charge, to any person obtaining a copy
-                            of this software and associated documentation files (the "Software"), to deal
-                            in the Software without restriction, including without limitation the rights
-                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                            copies of the Software, and to permit persons to whom the Software is
-                            furnished to do so, subject to the following conditions:<br/><br/>
+                <LicenseCard
+                    licenseName="Material-UI"
+                    href="https://github.com/mui-org/material-ui"
+                >
+                    Copyright (c) 2014 Call-Em-All
+                </LicenseCard>
 
-                            The above copyright notice and this permission notice shall be included in
-                            all copies or substantial portions of the Software.<br/><br/>
+                <LicenseCard
+                    licenseName="JWT-Decode"
+                    href="https://github.com/auth0/jwt-decode"
+                >
 
-                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-                            THE SOFTWARE.
-                        </Typography>
-                    </div>
-                </Paper>
+                    Copyright (c) 2015 Auth0, Inc. {`<support@auth0.com>`} (http://auth0.com)
+                </LicenseCard>
 
-                <Paper className="paper">
-                    <div className="license-name">
-                        <Typography variant="headline" color="secondary">
-                            Material-UI ( <a rel="noopener noreferrer" target="_blank" href="https://github.com/mui-org/material-ui">GitHub</a>)
-                        </Typography>
-                    </div>
+                <LicenseCard
+                    licenseName="React-Router"
+                    href="https://github.com/ReactTraining/react-router"
+                >
+                    Copyright (c) React Training 2016-2018
+                </LicenseCard>
 
-                    <div className="license-text">
-                        <Typography variant="caption">
-                            The MIT License (MIT)<br/><br/>
-
-                            Copyright (c) 2014 Call-Em-All<br/><br/>
-
-                            Permission is hereby granted, free of charge, to any person obtaining a copy
-                            of this software and associated documentation files (the "Software"), to deal
-                            in the Software without restriction, including without limitation the rights
-                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                            copies of the Software, and to permit persons to whom the Software is
-                            furnished to do so, subject to the following conditions:<br/><br/>
-
-                            The above copyright notice and this permission notice shall be included in all
-                            copies or substantial portions of the Software.<br/><br/>
-
-                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                            SOFTWARE.
-                        </Typography>
-                    </div>
-                </Paper>
-
-                <Paper className="paper">
-                    <div className="license-name">
-                        <Typography variant="headline" color="secondary">
-                            JWT-Decode ( <a rel="noopener noreferrer" target="_blank" href="https://github.com/auth0/jwt-decode">GitHub</a>)
-                        </Typography>
-                    </div>
-
-                    <div className="license-text">
-                        <Typography variant="caption">
-                            The MIT License (MIT) <br/><br/>
-
-                            Copyright (c) 2015 Auth0, Inc. {`<support@auth0.com>`} (http://auth0.com)<br/><br/>
-
-                            Permission is hereby granted, free of charge, to any person obtaining a copy
-                            of this software and associated documentation files (the "Software"), to deal
-                            in the Software without restriction, including without limitation the rights
-                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                            copies of the Software, and to permit persons to whom the Software is
-                            furnished to do so, subject to the following conditions:<br/><br/>
-
-                            The above copyright notice and this permission notice shall be included in all
-                            copies or substantial portions of the Software.<br/><br/>
-
-                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                            SOFTWARE.
-                        </Typography>
-                    </div>
-                </Paper>
-
-                <Paper className="paper">
-                    <div className="license-name">
-                        <Typography variant="headline" color="secondary">
-                            React-Router ( <a rel="noopener noreferrer" target="_blank" href="https://github.com/ReactTraining/react-router">GitHub</a>)
-                        </Typography>
-                    </div>
-
-                    <div className="license-text">
-                        <Typography variant="caption">
-                            MIT License <br/><br/>
-
-                            Copyright (c) React Training 2016-2018<br/><br/>
-
-                            Permission is hereby granted, free of charge, to any person obtaining a copy
-                            of this software and associated documentation files (the "Software"), to deal
-                            in the Software without restriction, including without limitation the rights
-                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                            copies of the Software, and to permit persons to whom the Software is
-                            furnished to do so, subject to the following conditions:<br/><br/>
-
-                            The above copyright notice and this permission notice shall be included in all
-                            copies or substantial portions of the Software.<br/><br/>
-
-                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                            SOFTWARE.
-                        </Typography>
-                    </div>
-                </Paper>
-
-                <Paper className="paper">
-                    <div className="license-name">
-                        <Typography variant="headline" color="secondary">
-                            React-Scroll-Up ( <a rel="noopener noreferrer" target="_blank" href="https://github.com/milosjanda/react-scroll-up">GitHub</a>)
-                        </Typography>
-                    </div>
-
-                    <div className="license-text">
-                        <Typography variant="caption">
-                            The MIT License (MIT)<br/><br/>
-
-                            Copyright (c) 2015 Miloš Janda<br/><br/>
-
-                            Permission is hereby granted, free of charge, to any person obtaining a copy
-                            of this software and associated documentation files (the "Software"), to deal
-                            in the Software without restriction, including without limitation the rights
-                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                            copies of the Software, and to permit persons to whom the Software is
-                            furnished to do so, subject to the following conditions:<br/><br/>
-
-                            The above copyright notice and this permission notice shall be included in all
-                            copies or substantial portions of the Software.<br/><br/>
-
-                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                            SOFTWARE.
-                        </Typography>
-                    </div>
-                </Paper>
+                <LicenseCard
+                    licenseName="React-Scroll-Up"
+                    href="https://github.com/milosjanda/react-scroll-up"
+                >
+                    Copyright (c) 2015 Miloš Janda
+                </LicenseCard>
             </div>
         );
     }
